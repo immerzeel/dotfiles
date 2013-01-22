de39039ff562506663f4b1b514bc310432d67cb4
Pascal Immerzeel <pascal@immerzeelpictures.com>
2013-01-22 11:19:52 +0100

    Added a first draft of an installation script.

---
+++ install.sh
@@ -0,0 +1,8 @@
+ln -s  dotfiles/bash_profile ~/.bash_profile
+ln -s  dotfiles/ctags ~/.ctags
+ln -s  dotfiles/gitconfig ~/.gitconfig
+ln -s  dotfiles/profile ~/.profile
+ln -s  dotfiles/rsyncignore ~/.rsyncignore
+ln -s  dotfiles/zprofile ~/.zprofile
+ln -s  dotfiles/zshrc ~/.zshrc
+
